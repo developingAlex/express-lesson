9ed68d43d3fc61996ae31b0d2c2b14c8e8a8a81f
Alex Palma <alex-palma@outlook.com>
2017-11-23 10:49:32 +1100

    Add usage of tickets.js file in server.js file.

--- src/server.js
+++ src/server.js
@@ -2,12 +2,16 @@ const express = require('express')
 
 const server = express()
 
+const ticketsRouter = require('./routes/tickets') //we don't need to mention ./routes/tickets.js because express expects it to be .js, it's implied.
+
+
 server.get('/', (req, res) => {
   res.json({ message: 'This is home' })
 })
 server.get('/about', (req, res) => {
   res.json({ message: 'This is about' })
 })
+server.use('/', ticketsRouter)
 
 server.listen(8001, () => {
   console.log('Startd at http://localhost:8001');
