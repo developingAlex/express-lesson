96a64eedfded1033ed89ea55bb74f8d400a7c869
Alex Palma <alex-palma@outlook.com>
2017-11-23 11:47:29 +1100

    Add id params parsing for returning individual animal

--- src/models/animal.js
+++ src/models/animal.js
@@ -1,31 +1,48 @@
 
 let animals = [
   {
+    id: 1,
     name: 'Meercat',
     count: 27
   },
   {
+    id: 2,
     name: 'White Rhinocerous',
     count: 1
   },
   {
+    id: 3,
     name: 'Peacocks',
     count: 14
   },
   {
+    id: 4,
     name: 'Giraffe',
     count: 4
   },
   {
+    id: 5,
     name: 'Tasmanian Tiger',
     count: 0
   },
 ]
+function find(id){
+  id = parseInt(id, 10) //the 10 means base 10: decimal
+  let foundAnimal = null
+  animals.forEach((animal) => {
+    if (animal.id === id){
+      foundAnimal = animal
+      // break
+    }
+  })
+  return foundAnimal
+}
 
 function all(){
   return animals
 }
 
 module.exports = {
-  all
+  all,
+  find
 }
--- src/routes/animals.js
+++ src/routes/animals.js
@@ -7,4 +7,9 @@ router.get('/animals', (req,res) => {
   res.json(Animal.all())
 })
 
+router.get('/animals/:id', (req, res) => {
+  const id = req.params['id']
+  res.json(Animal.find(id))
+})
+
 module.exports = router // to allow this to be used externally by the other files.
