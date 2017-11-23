e02016373b8b6d67621d62ae867439df897132f6
Alex Palma <alex-palma@outlook.com>
2017-11-23 11:16:27 +1100

    Refactor server.js code

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 const express = require('express')
 
 const server = express()
-const ticketsRouter = require('./routes/tickets') //we don't need to mention ./routes/tickets.js because express expects it to be .js, it's implied.
-const animalsRouter = require('./routes/animals')
-
 
 server.get('/', (req, res) => {
   res.json({ message: 'This is home' })
@@ -11,8 +8,11 @@ server.get('/', (req, res) => {
 server.get('/about', (req, res) => {
   res.json({ message: 'This is about' })
 })
-server.use('/', ticketsRouter) //the '/' part just declares that there is not going to be a prefix to the routes mentioned by ticketsRouter...
-server.use('/', animalsRouter)
+server.use('/', [
+  require('./routes/tickets'), 
+  require('./routes/animals')
+]) //the '/' part just declares that there is not going to be a prefix to the routes mentioned by ticketsRouter...
+//we don't need to mention ./routes/tickets.js because express expects it to be .js, it's implied.
 server.listen(8001, () => {
   console.log('Started at http://localhost:8001');
 })
