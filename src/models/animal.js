b007ba416d394a2fd38a5ca75f7056eb96664ab1
Alex Palma <alex-palma@outlook.com>
2017-11-23 11:36:11 +1100

    Refactor animals to come from its own file simulating mongodb

---
+++ src/models/animal.js
@@ -0,0 +1,31 @@
+
+let animals = [
+  {
+    name: 'Meercat',
+    count: 27
+  },
+  {
+    name: 'White Rhinocerous',
+    count: 1
+  },
+  {
+    name: 'Peacocks',
+    count: 14
+  },
+  {
+    name: 'Giraffe',
+    count: 4
+  },
+  {
+    name: 'Tasmanian Tiger',
+    count: 0
+  },
+]
+
+function all(){
+  return animals
+}
+
+module.exports = {
+  all
+}
--- src/routes/animals.js
+++ src/routes/animals.js
@@ -1,34 +1,10 @@
 const express = require('express')
 
+const Animal = require('../models/animal')
 const router = express.Router()
 
-let animals = [
-  {
-    name: 'Meercat',
-    count: 27
-  },
-  {
-    name: 'White Rhinocerous',
-    count: 1
-  },
-  {
-    name: 'Peacocks',
-    count: 14
-  },
-  {
-    name: 'Giraffe',
-    count: 4
-  },
-  {
-    name: 'Tasmanian Tiger',
-    count: 0
-  },
-]
-
-
-
 router.get('/animals', (req,res) => {
-  res.json(animals)
+  res.json(Animal.all())
 })
 
 module.exports = router // to allow this to be used externally by the other files.
